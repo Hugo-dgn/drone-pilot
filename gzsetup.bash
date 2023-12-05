6e82af668f78fd54b16d70dcc88bbabd7f93c8e8
Hugo Degeneve <hugo.degeneve@gmail.com>
2023-12-05 14:49:09 +0100

    path to firmware can be change

--- gzsetup.bash
+++ gzsetup.bash
@@ -1,3 +1,5 @@
-source ~/src/Firmware/Tools/simulation/gazebo-classic/setup_gazebo.bash ~/src/Firmware ~/src/Firmware/build/px4_sitl_default
+path_to_firmware=~/src/Firmware
 
-export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/src/Firmware:~/src/Firmware/Tools/simulation/gazebo-classic/sitl_gazebo-classic
+source $path_to_firmware/Tools/simulation/gazebo-classic/setup_gazebo.bash $path_to_firmware $path_to_firmware/build/px4_sitl_default
+
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:$path_to_firmware:$path_to_firmware/Tools/simulation/gazebo-classic/sitl_gazebo-classic
