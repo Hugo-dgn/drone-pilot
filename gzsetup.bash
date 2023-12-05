f9cd10f8aa625f6f893170a7d6b54fd9e70aed7c
Hugo Degeneve <hugo.degeneve@gmail.com>
2023-12-05 09:39:10 +0100

    setup files

---
+++ gzsetup.bash
@@ -0,0 +1,4 @@
+source ~/src/Firmware/Tools/simulation/gazebo-classic/setup_gazebo.bash ~/src/Firmware ~/src/Firmware/build/px4_sitl_default
+
+export ROS_PACKAGE_PATH=$ROS_PACKAGE_PATH:~/src/Firmware:~/src/Firmware/Tools/simulation/gazebo-classic/sitl_gazebo-classic
+
